012f111801d9827e0c7809f6d30d16c8d949bd25
Ryan C. Gordon <icculus@icculus.org>
2001-04-04 09:34:18 +0000

    New TurtleSpace architecture in place.

--- last/toby/turtlespace/TurtleSpace.java
+++ last/toby/turtlespace/TurtleSpace.java
@@ -19,51 +19,363 @@
 
 package last.toby.turtlespace;
 
-import last.toby.exceptions.ExecException;
+import java.awt.*;
+import last.toby.exceptions.*;
+import last.toby.util.*;
 
     /**
      *  !!! comment.
      *   @author Ryan C. Gordon. (icculus@lokigames.com)
      */
-public interface TurtleSpace
+public class TurtleSpace
 {
-    public abstract void grabTurtleSpace();
-    public abstract void releaseTurtleSpace();
-
-    public abstract double getTurtleSpaceWidth();
-    public abstract double getTurtleSpaceHeight();
-    public abstract double getDesiredTurtleWidth();
-    public abstract double getDesiredTurtleHeight();
-
-    public abstract float getDefaultPenRed();
-    public abstract float getDefaultPenBlue();
-    public abstract float getDefaultPenGreen();
+    private Turtle[] turtles = null;
+    private java.util.Stack turtlePool = new java.util.Stack();
+    private boolean hasFence = false;
+    private Turtle turtle = null;      // Current turtle.
+    private TurtleSpaceRenderer renderer = null;
 
 // !!! Would like an alpha channel.
-
-    public abstract int getTurtleCount();
-    public abstract int addTurtle();
-    public abstract boolean removeTurtle(int turtleIndex);
-    public abstract void removeAllTurtles();
-
-    public abstract void setFence(boolean buildFence) throws ExecException;
-    public abstract void enableFence() throws ExecException;
-    public abstract void disableFence() throws ExecException;
-
-    public abstract void useTurtle(int turtleIndex)  throws ExecException;
-
-    public abstract double getTurtleX()  throws ExecException;
-    public abstract double getTurtleY()  throws ExecException;
-    public abstract double getTurtleWidth()  throws ExecException;
-    public abstract double getTurtleHeight() throws ExecException;
-    public abstract void setTurtleX(double x) throws ExecException;
-    public abstract void setTurtleY(double y) throws ExecException;
-    public abstract void setTurtleXY(double x, double y)  throws ExecException;
-    public abstract void setTurtleWidth(double newWidth) throws ExecException;
-    public abstract void setTurtleHeight(double newHeight) throws ExecException;
-
-
 // !!! Would like a way to redefine the coordinate system of TurtleSpace.
+
+    public TurtleSpace(TurtleSpaceRenderer _renderer)
+    {
+        this.renderer = _renderer;
+    } // Constructor
+
+    public void grabTurtleSpace()
+    {
+        renderer.notifyGrabbed();
+    } // grabTurtleSpace
+
+    public void releaseTurtleSpace()
+    {
+        renderer.notifyUngrabbed();
+    } // grabTurtleSpace
+
+    protected synchronized Turtle getTurtleByIndex(int turtleIndex)
+    {
+        Turtle retVal = null;
+
+        if (turtles != null)
+        {
+            if ((turtleIndex >= 0) && (turtleIndex < turtles.length))
+                retVal = turtles[turtleIndex];
+        } // if
+
+        return(retVal);
+    } // getTurtleByIndex
+
+
+    public synchronized int getTurtleCount()
+    {
+        int retVal = 0;
+
+        if (turtles != null)
+        {
+            for (int i = 0; i < turtles.length; i++)
+            {
+                if (turtles[i] != null)
+                    retVal++;
+            } // for
+        } // if
+
+        return(retVal);
+    } // getTurtleCount
+
+
+    protected synchronized Turtle buildNewTurtle() throws ExecException
+    {
+        Turtle retVal = null;
+
+        if (turtlePool.isEmpty() == false)
+            retVal = (Turtle) turtlePool.pop();
+        else
+            retVal = new Turtle();
+
+        defaultTurtle(retVal);
+
+        return(retVal);
+    } // buildNewTurtle
+
+
+    public synchronized int addTurtle() throws ExecException
+    {
+        Turtle newTurtle = buildNewTurtle();
+        int max = 0;
+
+        if (turtles != null)
+        {
+            max = turtles.length;
+            for (int i = 0; i < max; i++)
+            {
+                if (turtles[i] == null)
+                {
+                    turtles[i] = newTurtle;
+                    return(i);
+                } // if
+            } // for
+        } // if
+
+            // if we landed here, we need to increase the size of the array...
+
+        Turtle newArray[] = new Turtle[max + 1];
+
+        if (turtles != null)
+            System.arraycopy(turtles, 0, newArray, 0, max);
+
+        newArray[max] = newTurtle;
+        turtles = newArray;
+        return(max);
+    } // addTurtle
+
+
+    public synchronized boolean removeTurtle(int turtleIndex) throws ExecException
+    {
+        boolean retVal = false;
+        Turtle t = getTurtleByIndex(turtleIndex);
+
+        if (turtle == t)
+            this.turtle = null;
+
+        if (t != null)
+        {
+            renderer.blankTurtle(t);
+            t.setVisible(false);
+            turtlePool.push(t);
+            turtles[turtleIndex] = null;
+            retVal = true;
+        } // if
+
+        return(retVal);
+    } // removeTurtle
+
+
+    public synchronized void removeAllTurtles() throws ExecException
+    {
+        if (turtles != null)
+        {
+            int max = turtles.length;
+
+            for (int i = 0; i < max; i++)
+                removeTurtle(i);
+        } // if
+    } // removeAllTurtles
+
+
+    public void enableFence() throws ExecException
+    {
+        setFence(true);
+    } // setFence
+
+
+    public void disableFence() throws ExecException
+    {
+        setFence(false);
+    } // setFence
+
+
+    public synchronized void setFence(boolean buildFence) throws ExecException
+    {
+        this.hasFence = buildFence;
+
+        if ((buildFence == true) && (turtles != null))
+        {
+            Turtle t;
+
+            for (int i = 0; i < turtles.length; i++)
+            {
+                t = turtles[i];
+                if (t != null)
+                    verifyTurtleInsideFence(t.getX(),t.getY());
+            } // for
+        } // if
+    } // setFence
+
+
+    protected synchronized void verifyTurtleInsideFence(double x, double y)
+                                                         throws ExecException
+    {
+        if (this.hasFence == true)
+        {
+            if ( (x < 0.00) || (x > renderer.getTurtleSpaceWidth()) ||
+                 (y < 0.00) || (y > renderer.getTurtleSpaceHeight()) )
+            {
+                ExecException._throw(TobyLanguage.TURTLE_FENCED);
+            } // if
+        } // if
+    } // verifyTurtleInsideFence
+
+
+    public synchronized void useTurtle(int turtleIndex) throws ExecException
+    {
+        Turtle newTurtle = getTurtleByIndex(turtleIndex);
+        if (newTurtle == null)
+            ExecException._throw(TobyLanguage.BAD_ARGUMENT);
+        else
+            this.turtle = newTurtle;
+    } // useTurtle
+
+
+    public synchronized double getTurtleX() throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+        return(this.turtle.getX());
+    } // getTurtleX
+
+
+    public synchronized double getTurtleY() throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+        return(turtle.getY());
+    } // getTurtleY
+
+
+    public synchronized double getTurtleWidth() throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+        return(turtle.getWidth());
+    } // getTurtleWidth
+
+
+    public synchronized double getTurtleHeight() throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+        return(turtle.getHeight());
+    } // getTurtleHeight
+
+
+    public synchronized void setTurtleX(double x) throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+
+        if (x != turtle.getX())
+        {
+            renderer.blankTurtle(turtle);
+            turtle.setX(x);
+            renderer.renderTurtle(turtle);
+            verifyTurtleInsideFence(x, turtle.getY());
+        } // if
+    } // setTurtleX
+
+
+    public synchronized void setTurtleY(double y) throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+
+        if (y != turtle.getY())
+        {
+            renderer.blankTurtle(turtle);
+            turtle.setY(y);
+            renderer.renderTurtle(turtle);
+            verifyTurtleInsideFence(turtle.getX(), y);
+        } // if
+    } // setTurtleY
+
+
+    public synchronized void setTurtleXY(double x, double y) throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+
+        if ((y != turtle.getY()) || (x != turtle.getX()))
+        {
+            renderer.blankTurtle(turtle);
+            turtle.setXY(x, y);
+            renderer.renderTurtle(turtle);
+            verifyTurtleInsideFence(x, y);
+        } // if
+    } // setTurtleXY
+
+
+    public synchronized void setTurtleWidth(double newWidth) throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+
+        if (newWidth != turtle.getWidth())
+        {
+            renderer.blankTurtle(turtle);
+            turtle.setWidth(newWidth);
+            renderer.renderTurtle(turtle);
+        } // if
+    } // setTurtleWidth
+
+
+    public synchronized void setTurtleHeight(double newHeight) throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+
+        if (newHeight != turtle.getHeight())
+        {
+            renderer.blankTurtle(turtle);
+            turtle.setHeight(newHeight);
+            renderer.renderTurtle(turtle);
+        } // if
+    } // setTurtleHeight
+
+    protected synchronized void setTurtleVisibility(Turtle t, boolean isVis) throws ExecException
+    {
+        if (isVis != t.getVisible())
+        {
+            t.setVisible(isVis);
+
+            if (isVis)
+                renderer.renderTurtle(t);
+            else
+                renderer.blankTurtle(t);
+        } // if
+    } // setTurtleVisibility
+
+    public synchronized void setTurtleVisibility(boolean isVis) throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+
+        setTurtleVisibility(turtle, isVis);
+    } // setTurtleVisible
+        
+    public synchronized void showTurtle() throws ExecException
+    {
+        setTurtleVisibility(true);
+    } // showTurtle
+
+    public synchronized void showAllTurtles() throws ExecException
+    {
+        for (int i = 0; i < turtles.length; i++)
+        {
+            if (turtles[i] != null)
+                setTurtleVisibility(turtles[i], true);
+        } // for
+    } // showAllTurtles
+
+    public synchronized void hideTurtle() throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+
+        setTurtleVisibility(turtle, false);
+    } // hideTurtle
+
+    public synchronized void hideAllTurtles() throws ExecException
+    {
+        for (int i = 0; i < turtles.length; i++)
+        {
+            if (turtles[i] != null)
+                setTurtleVisibility(turtles[i], false);
+        } // for
+    } // hideAllTurtles
+
+    public float getDefaultPenRed()    { return((float) 1.0); }
+    public float getDefaultPenBlue()   { return((float) 1.0); }
+    public float getDefaultPenGreen()  { return((float) 1.0); }
 
 
         /**
@@ -75,25 +387,170 @@ public interface TurtleSpace
          *
          *    @param distance How far to move the turtle.
          */
-    public abstract void advanceTurtle(double distance) throws ExecException;
-    public abstract void rotateTurtle(double degree) throws ExecException;
-    public abstract void setTurtleAngle(double angle) throws ExecException;
-    public abstract void setPenColor(float r, float g, float b) throws ExecException;
-    public abstract void setPen(boolean drawing) throws ExecException;
-    public abstract void setPenUp() throws ExecException;
-    public abstract void setPenDown() throws ExecException;
-    public abstract void drawString(String str) throws ExecException;
-    public abstract void homeTurtle() throws ExecException;
-    public abstract void homeAllTurtles() throws ExecException;
-    public abstract void setTurtleVisibility(boolean isVis) throws ExecException;
-    public abstract void showTurtle() throws ExecException;
-    public abstract void showAllTurtles() throws ExecException;
-    public abstract void hideTurtle() throws ExecException;
-    public abstract void hideAllTurtles() throws ExecException;
-    public abstract void defaultTurtle() throws ExecException;
-    public abstract void defaultAllTurtles() throws ExecException;
+    public synchronized void advanceTurtle(double distance) throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
 
-    public abstract void cleanup();
+        DoublePoint point;
+
+        double angle = turtle.getAngle();
+        double turtleX = turtle.getX();
+        double turtleY = turtle.getY();
+
+        point = TobyGeometry.calculateLine(angle, distance, turtleX, turtleY);
+
+        renderer.blankTurtle(turtle);
+
+        if (turtle.isPenDown())   // draw the line covering path turtle took?
+        {
+            renderer.renderLine(turtleX, turtleY,
+                                point.x, point.y, turtle.getPenColor());
+        } // if
+
+        turtle.setXY(point.x, point.y);
+        renderer.renderTurtle(turtle);
+        verifyTurtleInsideFence(point.x, point.y);
+    } // advanceTurtle
+
+
+    public synchronized void setTurtleAngle(double angle) throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+
+        renderer.blankTurtle(turtle);
+        turtle.setAngle(angle);
+        renderer.renderTurtle(turtle);
+    } // setTurtleAngle
+
+
+    public synchronized void rotateTurtle(double degree) throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+        setTurtleAngle(turtle.getAngle() + degree);
+    } // rotateTurtle
+
+
+    public synchronized void setPenColor(float r, float g, float b) throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+        turtle.setPenColor(r, g, b);
+    } // setPenColor
+
+
+    public void setPen(boolean drawing) throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+        turtle.setPenDown(drawing);
+    } // setPen
+
+
+    public void setPenUp() throws ExecException
+    {
+        setPen(false);
+    } // setPenUp
+
+
+    public void setPenDown() throws ExecException
+    {
+        setPen(true);
+    } // setPenDown
+
+
+    protected synchronized void homeTurtle(Turtle t) throws ExecException
+    {
+        int x = TobyGeometry.roundDoubleToInt(renderer.getTurtleSpaceWidth() / 2);
+        int y = TobyGeometry.roundDoubleToInt(renderer.getTurtleSpaceHeight() / 2);
+
+        renderer.blankTurtle(t);
+        t.setXY(x, y);
+        renderer.renderTurtle(t);
+    } // homeTurtle (overload; takes a Turtle object)
+
+
+    public synchronized void homeTurtle() throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+        homeTurtle(turtle);
+    } // homeTurtle (overload; public version that uses current turtle.)
+
+
+    public synchronized void homeAllTurtles() throws ExecException
+    {
+        for (int i = 0; i < turtles.length; i++)
+        {
+            if (turtles[i] != null)
+                homeTurtle(turtles[i]);
+        } // for
+    } // homeAllTurtles
+
+
+    protected synchronized void defaultTurtle(Turtle t) throws ExecException
+    {
+        if (t != null)
+        {
+            float r = getDefaultPenRed();
+            float g = getDefaultPenGreen();
+            float b = getDefaultPenBlue();
+
+            renderer.blankTurtle(t);
+            t.setAngle(Turtle.DUE_NORTH);
+            t.setPenDown(true);
+            t.setPenColor(r, g, b);
+            t.setVisible(true);
+            homeTurtle(t);
+            renderer.renderTurtle(t);
+        } // if
+    } // defaultTurtle (overload; takes a Turtle object)
+
+
+    public synchronized void defaultTurtle() throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+        defaultTurtle(turtle);
+    } // defaultTurtle (overload; public version that uses current turtle.)
+
+
+    public synchronized void defaultAllTurtles() throws ExecException
+    {
+        if (turtles != null)
+        {
+            for (int i = 0; i < turtles.length; i++)
+                defaultTurtle(turtles[i]);
+        } // if
+    } // defaultAllTurtles
+
+
+    public void drawString(String str) throws ExecException
+    {
+        if (turtle == null)
+            ExecException._throw(TobyLanguage.NOCURTURTLE);
+
+        renderer.renderString(turtle.getX(), turtle.getY(), turtle.getAngle(),
+                               turtle.getPenColor(), str);
+    } // renderString
+
+    public void cleanup() throws ExecException
+    {
+        renderer.cleanup();
+    } // cleanup
+
+    public double getTurtleSpaceWidth()
+    {
+        return(renderer.getTurtleSpaceWidth());
+    } // getTurtleSpaceWidth
+
+    public double getTurtleSpaceHeight()
+    {
+        return(renderer.getTurtleSpaceHeight());
+    } // getTurtleSpaceWidth
+
 } // TurtleSpace
 
 // end of TurtleSpace.java ...
