039df47fdd345fdbf38a11455b73f4f87f565b30
Ryan C. Gordon <icculus@icculus.org>
2001-04-22 04:59:43 +0000

    Initial add.

---
+++ src/parsers/Parser.cpp
@@ -0,0 +1,157 @@
+/*
+ * TOBY -- An abstract interpreter engine and system for learning.
+ * Copyright (C) 1999  Ryan C. Gordon.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "parsers/Parser.h"
+
+Parser::Parser(void)
+    : totalParseTime(0xdeadbeef),
+      totalLinkTime(0xdeadbeef)
+{
+} // Constructor
+
+
+Parser::~Parser(void)
+{
+} // Destructor
+
+
+    // !!! Change this bubble sort with something faster?
+TobyCollection *Parser::alphabetizeArrayElements(TobyCollection *array)
+{
+    int i;
+    int max;
+    bool sorted;
+
+    if (array == NULL)      // safety check.
+        return(NULL);
+
+    max = array->size() - 1;
+
+    if (max <= 0)
+        return(array);
+
+    do
+    {
+        sorted = true;
+        for (i = 0; i < max; i++)
+        {
+            char *str1 = (char *) array->elementAt(i);
+            char *str2 = (char *) array->elementAt(i + 1);
+            if (strcmp(str1, str2) > 0)
+            {
+                sorted = false;
+                array->swapElements(i, i + 1);
+            } // if
+        } // for
+    } while (sorted == false);
+
+    return(array);
+} // Parser::alphabetizeArrayElements
+
+
+// !!! this is a slow algorithm.
+int Parser::searchAlphabeticArray(TobyCollection *table, const char *forThis)
+{
+    if (forThis != NULL)   // bullet proofing.
+    {
+        int max = table->size();
+        for (int i = 0; i < max; i++)
+        {
+            int rc = strcmp(forThis, (char *) (table->elementAt(i)));
+            if (rc == 0)
+                return(i);
+            else if (rc < 0)  // alphabetically past search string?
+                return(-1);
+        } // for
+    } // if
+
+    return(-1);   // not found. Bail.
+} // Parser::searchAlphabeticArray
+
+
+int Parser::searchArray(TobyCollection *table, const char *forThis)
+{
+    if (forThis != NULL)
+    {
+        int max = table->size();
+        for (int i = 0; i < max; i++)
+        {
+            if (strcmp((char *) table->elementAt(i), forThis) == 0)
+                return(i);
+        } // for
+    } // if
+
+    return(-1);
+} // Parser::searchArray
+
+
+int Parser::searchArrayForReference(TobyCollection *table, const char *str)
+{
+    int max = table->size();
+
+    for (int i = 0; i < max; i++)
+    {
+        if (table->elementAt(i) == (void *) str)
+            return(i);
+    } // for
+
+    return(-1);
+} // Parser::searchArrayForReference
+
+
+GlobalLogicContext *Parser::parseCode(TobyReader *code, TobyObject *screen)
+                                                throw (ParseException *)
+{
+    _D(("Parser::parseCode(): Starting parsing phase...\n"));
+    TobyClock *clock = __platformGetSingletonClock();
+    toby_uint32 parseStartTime = clock->getTicks();
+    GlobalLogicContext *retVal = parseCodeImpl(code);
+    totalParseTime = clock->getTicks() - parseStartTime;
+
+    if (retVal != NULL)
+    {
+        // !!! retVal->setScreen(screen);
+
+        _D(("Parser::parseCode(): Starting linking phase...\n"));
+        toby_uint32 linkStartTime = clock->getTicks();
+        // !!! retVal->link(retVal);
+        totalLinkTime = clock->getTicks() - linkStartTime;
+    } // if
+
+    _D(("Parser::parseCode(): Total parse time: (%lu) ms.\n", totalParseTime));
+    _D(("Parser::parseCode(): Total link  time: (%lu) ms.\n", totalLinkTime));
+
+    return(retVal);
+} // Parser::parseCode
+
+
+toby_uint32 Parser::getTotalParseTime(void)
+{
+    return(totalParseTime);
+} // Parser::getTotalParseTime
+
+
+toby_uint32 Parser::getTotalLinkTime(void)
+{
+    return(totalLinkTime);
+} // Parser::getTotalLinkTime
+
+
+// end of Parser.cpp ...
+
---
+++ src/parsers/Parser.h
@@ -0,0 +1,115 @@
+/*
+ * TOBY -- An abstract interpreter engine and system for learning.
+ * Copyright (C) 1999  Ryan C. Gordon.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef _INCLUDE_PARSER_H_
+#define _INCLUDE_PARSER_H_
+
+#include <string.h>
+#include "util/TobyObject.h"
+#include "util/TobyCollection.h"
+#include "exceptions/ParseException.h"
+#include "util/TobyClock.h"
+#include "io/TobyReader.h"
+
+// !!!
+typedef void GlobalLogicContext;
+
+
+/*
+ * This abstract base class supplies the interface for various language
+ *  parsers to inplement. Examples of subclasses are TobyParser, LogoParser,
+ *  etc. The parser's goal is to take a stream of tokens and build it into
+ *  a tree of LogicContexts, of which can then be executed as a complete
+ *  program.
+ *
+ *     Written by Ryan C. Gordon. (icculus@linuxgames.com)
+ */
+class Parser : public TobyObject
+{
+public:
+
+    Parser(void);
+    virtual ~Parser(void);
+
+        // A simple bubble sort to alphabetize an array of strings.
+        //  returns the same array that was passed in, but the contents
+        //  of the array has been shifted about.
+    static TobyCollection *alphabetizeArrayElements(TobyCollection *array);
+
+
+        // Find out if (forThis) is in (table). The strings contained in table
+        //  must be indexed in alphabetic order. This makes the searches
+        //  faster.
+    static int searchAlphabeticArray(TobyCollection *table, const char *str);
+
+
+        // Return index of (forThis) in (table), or (-1) if it isn't there.
+    static int searchArray(TobyCollection *table, const char *forThis);
+
+
+        // Find out if (forThis) is in (table). (forThis) must not be just
+        //  an identical string, but an identical pointer.
+    static int searchArrayForReference(TobyCollection *table, const char *str);
+
+
+        // Call this to parse a collection of code into a LogicContext tree.
+        //  (code) may be an entire program or just a single procedure or
+        //  whatever else. DO NOT OVERRIDE THIS! Implement parseCodeImpl(),
+        //  instead, as this function does some setup before calling that
+        //  for the actual parsing.
+        //
+        //   params: code   The code to parse. This is usually a StringReader,
+        //                  but may come directly from disk or network.
+        //           screen An object representing the screen. This is
+        //                  language/platform, etc dependent, but in the
+        //                  usual/intended case, it's an implementor of
+        //                  last.toby.turtlespace.TurtleSpace.
+        //  returns: A new ModuleLogicContext containing procedures, global
+        //            vars, and other attributes resulting from the parsing of
+        //            (code).
+        //   throws: ParseException on syntax errors, etc...
+    GlobalLogicContext *parseCode(TobyReader *code, TobyObject *screen)
+                                                throw (ParseException *);
+
+        // Get the total time that was spent during the last parsing,
+        //  in milliseconds.
+    toby_uint32 getTotalParseTime(void);
+
+
+         // Get the total time that was spent during the last linking,
+         //  in milliseconds.
+    toby_uint32 getTotalLinkTime(void);
+
+
+         // This is the entry point for a given parser to do its work.
+         //  It returns a GlobalLogicContext, which is the root of a tree
+         //  containing the logic structure of the whole program, ready for
+         //  linking and then running.
+    virtual GlobalLogicContext *parseCodeImpl(TobyReader *code)
+                                                throw (ParseException *) = 0;
+
+protected:
+    toby_uint32 totalParseTime;
+    toby_uint32 totalLinkTime;
+}; // class Parser
+
+#endif  // !defined _INCLUDE_PARSER_H_
+
+// end of Parser.h ...
+
