f7a2b2a3fa58202587b75f09b7666bad43f71d3f
Ryan C. Gordon <icculus@icculus.org>
2007-01-15 12:52:49 +0000

    Temporary hack to fill in Toby native function stubs.

--- src/lua-5.1.1/src/linit.c
+++ src/lua-5.1.1/src/linit.c
@@ -13,6 +13,32 @@
 #include "lualib.h"
 #include "lauxlib.h"
 
+/* !!! FIXME: temporary hack. */
+#define LUAHOOK_INT(sym) \
+    static int luahook_##sym(lua_State *L) { \
+        const int i1 = luaL_checkinteger(L, 1); \
+        printf("\t%s(%d)\n", #sym, i1); \
+        return 0; \
+    }
+
+LUAHOOK_INT(goForward)
+LUAHOOK_INT(turnRight)
+LUAHOOK_INT(turnLeft)
+LUAHOOK_INT(setPenColor)
+
+static void add_toby_functions(lua_State *L)
+{
+    #define SET_LUAHOOK(sym) \
+        lua_pushcfunction(L, luahook_##sym); \
+        lua_setglobal(L, #sym);
+    SET_LUAHOOK(goForward);
+    SET_LUAHOOK(turnRight);
+    SET_LUAHOOK(turnLeft);
+    SET_LUAHOOK(setPenColor);
+    #undef SET_LUAHOOK
+}
+
+
 
 static const luaL_Reg lualibs[] = {
   {"", luaopen_base},
@@ -34,5 +60,6 @@ LUALIB_API void luaL_openlibs (lua_State *L) {
     lua_pushstring(L, lib->name);
     lua_call(L, 1, 0);
   }
+  add_toby_functions(L);
 }
 
